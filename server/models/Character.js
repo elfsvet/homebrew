3e55cb5faea7eba4f24d20df37badb22a8b1fa30
Julio.S <just.julio95@gmail.com>
2022-08-10 20:59:01 -0400

    created models for USER, CHARACTERS and COMMENT

---
+++ server/models/Character.js
@@ -0,0 +1,61 @@
+const { Schema, model } = require('mongoose');
+const commentSchema = require('./Comment');
+
+const characterSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: 'Please provide a name for this character',
+      minlength: 1,
+      maxlength: 10
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    strength: {
+      type: Number,
+      required: true,
+      enum: ['15', '14', '13', '12', '10', '8']
+    },
+    dexterity: {
+      type: Number,
+      required: true,
+      enum: ['15', '14', '13', '12', '10', '8']
+    },
+    constitution: {
+      type: Number,
+      required: true,
+      enum: ['15', '14', '13', '12', '10', '8']
+    },
+    intelligence: {
+      type: Number,
+      required: true,
+      enum: ['15', '14', '13', '12', '10', '8']
+    },
+    wisdom: {
+      type: Number,
+      required: true,
+      enum: ['15', '14', '13', '12', '10', '8']
+    },
+    charisma: {
+      type: Number,
+      required: true,
+      enum: ['15', '14', '13', '12', '10', '8']
+    },
+    comments: [commentSchema]
+  },
+  {
+    toJSON: {
+      getters: true
+    }
+  }
+)
+
+characterSchema.virtual('commentCount').get(function() {
+  return this.comments.length;
+})
+
+const Character = model('Character', characterSchema);
+
+module.exports = Character;
---
+++ server/models/Comment.js
@@ -0,0 +1,23 @@
+const { Schema } = require('mongoose');
+
+const commentSchema = new Schema(
+  {
+    commentBody: {
+      type: String,
+      required: true,
+      maxlength: 280
+    },
+    username: {
+      type: String,
+      required: true
+    }
+  },
+  {
+    toJSON: 
+    {
+      getters: true
+    }
+  }
+)
+
+module.exports = reactionSchema;
