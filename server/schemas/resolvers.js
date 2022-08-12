e28c7515ca0025dca745241c415dc915e0da9739
Julio.S <just.julio95@gmail.com>
2022-08-12 09:38:29 -0400

    added typedefs and completed resolvers. Verification issues with JWT persist...

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,12 +4,45 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
+    // query logged in user
+    // ISSUES WITH VERIFICATION
+    me: async(parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id })
+          .select('-__v -password')
+          .populate('characters')
+          .populate('partyMembers')
+        
+        return userData;
+      }
+      throw new AuthenticationError('Not Logged In')
+    },
+
     // query all users
     users: async () => {
       return User.find()
         .select('-__v -password')
         .populate('partyMembers')
         .populate('characters')
+    },
+
+    // query single user
+    user: async(parent, { username }) => {
+      return User.findOne({ username })
+        .select('-__v -password')
+        .populate('partyMembers')
+        .populate('characters')
+    },
+
+    // query all character builds
+    characters: async(parent, { username }) => {
+      const params = username ? { username } : {};
+      return Character.find(params)
+    },
+
+    // query single character
+    character: async(parent, { _id }) => {
+      return Character.findOne({ _id })
     }
   },
 
@@ -38,8 +71,50 @@ const resolvers = {
 
       const token = signToken(user);
       return { token, user }
+    },
+
+    // ISSUES WITH VERIFICATION
+    addCharacter: async(parent, args, context) => {
+      if (context.user) {
+        const character = await Character.create({ ... args, username: context.user.username });
+
+        await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $push: { characters: character._id } },
+          { new: true }
+        );
+        return character
+      }
+      throw new AuthenticationError('You need to be logged in to create a character')
+    },
+
+    // add comments
+    // ISSUES WITH VERIFICATION
+    addComment: async(parent, { characterId, commentBody }, context) => {
+      if(context.user) {
+        const updatedCharacter = await Character.findOneAndUpdate(
+          { _id: characterId },
+          { $push: { comments: { commentBody, username: context.user.username } } },
+          { new: true, runValidators: true}
+        );
+        return updatedCharacter
+      }
+    },
+
+    // add partymembers
+    addPartyMember: async(parent, { partyMemberId }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { partyMembers: partyMemberId } },
+          { new: true }
+        ).populate('partyMembers');
+
+        return updatedUser
+      }
+      throw new AuthenticationError('You need to be logged in')
     }
   }
 }
 
-module.exports = resolvers
+module.exports = resolvers;
