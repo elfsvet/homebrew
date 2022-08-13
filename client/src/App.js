d9ce8d53cd4a6a6b44869f32d5c96961f26dbc2f
Abstrack5 <armon_ah05@outlook.com>
2022-08-13 06:43:41 -0400

    installed apollo/client, jwt-decode, setup apollo client side,app working as intended, login form displays login failed with attempt, need to work on signup functionality next

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,59 @@
-import './App.css';
-import Home from '../src/pages/Home';
-import Login from '../src/pages/Login';
-import Signup from '../src/pages/Signup';
-import Profile from '../src/pages/Profile';
-import ViewBuild from '../src/pages/ViewBuild';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import "./App.css";
+import Home from "../src/pages/Home";
+import Login from "../src/pages/Login";
+import Signup from "../src/pages/Signup";
+import Profile from "../src/pages/Profile";
+import ViewBuild from "../src/pages/ViewBuild";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// connection to /graphql endpoint
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer  ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  // we use the ApolloClient() constructor to instantiate the Apollo Client
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className='App'>
+    <ApolloProvider client={client}>
       <Router>
+        <div className="App">
         <Navbar />
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/signup' element={<Signup />} />
-          <Route path='/viewbuilds' element={<ViewBuild />} />
-          <Route path='/profile' element={<Profile />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/viewbuilds" element={<ViewBuild />} />
+          <Route path="/profile" element={<Profile />} />
         </Routes>
         <Footer />
+        </div>
       </Router>
-    </div>
+    </ApolloProvider>
   );
 }
 
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,23 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { LOGIN_USER } from "../utils/mutations";
+import Auth from "../utils/auth";
 import "../styles/login.css";
 
-function Login() {
+function Login(props) {
+  const [formState, setFormState] = useState({ email: "", password: "" });
+
+  // update state based on form input changes
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  const [login, { error }] = useMutation(LOGIN_USER);
+
+  // submit form
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const { data } = await login({
+        variables: { ...formState },
+      });
+      Auth.login(data.login.token);
+    } catch (e) {
+      console.error(e);
+    }
+    // clear form values
+    setFormState({
+      email: "",
+      password: "",
+    });
+  };
+
   return (
     <div className="login-wrapper">
-      <form className="login-form">
-      <h2>Login</h2>
+      <form className="login-form" onSubmit={handleFormSubmit}>
+        <h2>Login</h2>
         <label>
-          <p>Username</p>
-          <input type="text" />
+          <p>Email</p>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            value={formState.email}
+            onChange={handleChange}
+          />
         </label>
         <label>
           <p>Password</p>
-          <input type="password" />
+          <input
+            type="password"
+            name="password"
+            id="password"
+            value={formState.password}
+            onChange={handleChange}
+          />
         </label>
         <div>
           <button type="submit">Submit</button>
         </div>
       </form>
+      {error && <div className="login-failed">Login failed!</div>}
     </div>
   );
 }
