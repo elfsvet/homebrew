781031d2db2fb00da0ade144ba01088c9542db44
Abstrack5 <armon_ah05@outlook.com>
2022-08-13 03:49:14 -0400

    added super basic login/signup form setup, no css

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,7 +1,25 @@
 import React from "react";
+import "../styles/login.css";
 
 function Login() {
-  return <div> Login </div>;
+  return (
+    <div className="">
+      <h2>Login</h2>
+      <form>
+        <label>
+          <p>Username</p>
+          <input type="text" />
+        </label>
+        <label>
+          <p>Password</p>
+          <input type="password" />
+        </label>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,7 +1,28 @@
 import React from "react";
 
 function Signup() {
-  return <div> Signup </div>;
+  return (
+    <div className="">
+      <h2>Signup</h2>
+      <form>
+        <label>
+          <p>Username</p>
+          <input type="text" />
+        </label>
+        <label>
+          <p>Email</p>
+          <input type="email" />
+        </label>
+        <label>
+          <p>Password</p>
+          <input type="password" />
+        </label>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default Signup;
