3a9a1d79c40d76fc5701c58da57c5642e74c4662
Abstrack <Armon_ah05@outlook.com>
2022-08-16 14:59:37 -0400

    apolloprovider placed

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
-import { Container } from 'react-bootstrap'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Header from './components/Header'
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
+import "./App.css";
+import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import Header from "./components/Header";
+
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer  ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
+    <ApolloProvider client={client}>
     <Router>
       <Header />
-      <main className='py-3'>
+      <main className="py-3">
         <Container>
-          <Routes>
-            {/* <Route path='/'element={<Header />}/> */}
-          </Routes>
+          <Routes>{/* <Route path='/'element={<Header />}/> */}</Routes>
         </Container>
       </main>
       {/* <Footer /> */}
     </Router>
+    </ApolloProvider>
   );
 }
 
