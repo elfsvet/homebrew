a35e487cdf668b1ba032edd104a3e49e6d62591c
elfsvet <stepanmatysik@gmail.com>
2022-08-17 17:36:06 -0400

    removed some comments

--- client/src/pages/ProfilePage.js
+++ client/src/pages/ProfilePage.js
@@ -47,34 +47,6 @@ const ProfilePage = () => {
       {/* USER's BUILDS */}
       <Col xs={12} md={4}>
         <CharacterList characters={user.characters} title={`${user.username}'s builds...`}/>
-        {/* <Container className='mb-3 '>
-          <Card className='mb-3 ' style={{ width: '100%' }}>
-            <Card.Body>
-              <Card.Title>Character Name</Card.Title>
-              <Card.Subtitle className='mb-2 text-muted'>
-                Race/Class
-              </Card.Subtitle>
-              <Card.Text>Character Bio</Card.Text>
-              <Card.Text> */}
-                {/* going to need a function for the stats to map through all the stats of the character to load */}
-                {/* <span>HP</span> | <span>Stats</span>
-              </Card.Text>
-            </Card.Body>
-          </Card> */}
-          {/* second card to delete, for show for now */}
-           {/* <Card style={{ width: '100%' }}>
-             <Card.Body>
-               <Card.Title>Character Name</Card.Title>
-               <Card.Subtitle className='mb-2 text-muted'>
-                 Race/Class
-               </Card.Subtitle>
-               <Card.Text>Character Bio</Card.Text>
-               <Card.Text>
-                 <span>HP</span> | <span>Stats</span>
-               </Card.Text>
-             </Card.Body>
-           </Card>
-         </Container> */}
       </Col>
       {/* BIO USER INFO AND CHANGE PROFILE FRIENDS BOOKMARK */}
       <Col xs={12} md={8}>
