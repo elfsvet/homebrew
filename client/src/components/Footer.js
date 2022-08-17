0d6a006b4e326babf57c3932aa45f2ff6ec76f57
elfsvet <stepanmatysik@gmail.com>
2022-08-16 21:12:22 -0400

    footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,11 +4,16 @@ import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
-    <Container>
+    <Container
+      className='pt-2'    >
       <Row className='text-center justify-content-space-between'>
-        <Col xs={12} md={6}  >Copyright &copy; {new Date().getFullYear()}</Col>
+        <Col xs={12} md={6}>
+          Copyright &copy; {new Date().getFullYear()}
+        </Col>
         {/* should be external links to our github's profiles */}
-        <Col xs={12} md={6} >Armon Julio Yolanda Stepan</Col>
+        <Col xs={12} md={6}>
+          Armon Julio Yolanda Stepan
+        </Col>
       </Row>
     </Container>
   )
