955b572feaea50b41a9296305ad04a68ee10b36f
elfsvet <stepanmatysik@gmail.com>
2022-08-18 22:02:34 -0400

    added single page handeling and added singleCharacter component to use later

---
+++ client/src/components/SingleCharacter.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Container, Card} from 'react-bootstrap'
+
+const SingleCharacter = ({ character }) => {
+  if (!character.length) {
+    return <h3>Huh... No one in the tavern...</h3>
+  }
+
+  return (
+    <Container>
+      <Card key={character._id} className='mb-3' style={{ width: '100%' }}>
+        {/* Card Header with character name */}
+        <Card.Header>
+          <Link
+            to={`/character/${character._id}`}
+            style={{ fontWeight: 700, textDecoration: 'none' }}
+          >
+            <Card.Title>{character.name}</Card.Title>
+          </Link>
+        </Card.Header>
+        <Card.Body>
+          {/* Card Body with username */}
+          <Link
+            to={`/profile/${character.username}`}
+            style={{ fontWeight: 700, textDecoration: 'none' }}
+          >
+            <span>
+              <Card.Subtitle className='mb-2 text-muted'>
+                {' '}
+                Created by: {character.username}
+              </Card.Subtitle>
+            </span>
+          </Link>
+          <Card.Text className='mb-2'>
+            {character.race}/{character.class}
+          </Card.Text>
+          <Card.Text>{character.bio}</Card.Text>
+          <Card.Text>
+            <span>
+              STR:{character.strength} | DEX:{character.dexterity} | CON:
+              {character.constitution} | INT:{character.intelligence} | WIS:
+              {character.wisdom} | CHA:{character.charisma}
+            </span>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </Container>
+  )
+}
+
+export default SingleCharacter
--- client/src/pages/CharacterInfoPage.js
+++ client/src/pages/CharacterInfoPage.js
@@ -1,21 +1,63 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
+import { Container, Card } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 import { QUERY_CHARACTER, QUERY_ME_BASIC } from '../utils/queries'
 import { useQuery } from '@apollo/client'
 import { useParams } from 'react-router-dom'
 import CharacterList from '../components/CharacterList'
 
 const CharacterInfoPage = () => {
-  const params = useParams()
-  const characterId = params.id
-  // const { loading, data} = useQuery(QUERY_CHARACTER)
+  const { id: charId } = useParams()
+  const { loading, data } = useQuery(QUERY_CHARACTER, {
+    variables: { id: charId },
+  })
+  console.log('This is data: ' + JSON.stringify(data))
+  const character = data?.character || []
   // const { data: userData } = useQuery(QUERY_ME_BASIC)
-  console.log(characterId)
+  console.log(charId)
+  console.log(character.name)
   // const character = data?.character || []
   // console.log(userData)
   return (
+    
     <Container>
-      {/* <CharacterList></CharacterList> */}
+      {/* <CharacterList ></CharacterList> */}
+      <Card key={character._id} className='mb-3'>
+        <Card.Header>
+          <Link
+            to={`/character/${character._id}`}
+            style={{ fontWeight: 700, textDecoration: 'none' }}
+          >
+            <Card.Title>{character.name}</Card.Title>
+          </Link>
+        </Card.Header>
+        {/* Card Header with character name */}
+        <Card.Body>
+              {/* Card Body with username */}
+              <Link
+                to={`/profile/${character.username}`}
+                style={{ fontWeight: 700, textDecoration: 'none' }}
+              >
+                <span>
+                  <Card.Subtitle className='mb-2 text-muted'>
+                    {' '}
+                    Created by: {character.username}
+                  </Card.Subtitle>
+                </span>
+              </Link>
+              <Card.Text className='mb-2'>
+                {character.race}/{character.class}
+              </Card.Text>
+              <Card.Text>{character.bio}</Card.Text>
+              <Card.Text>
+                <span>
+                  STR:{character.strength} | DEX:{character.dexterity} | CON:
+                  {character.constitution} | INT:{character.intelligence} | WIS:
+                  {character.wisdom} | CHA:{character.charisma}
+                </span>
+              </Card.Text>
+            </Card.Body>
+          </Card>
     </Container>
   )
 }
